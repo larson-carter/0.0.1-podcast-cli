66bef5b0da1ccd54898dbed61a63ad7c12de52a2
Bartek Pacia <barpac02@gmail.com>
2020-06-03 21:55:07 +0200

    basically the state of code when Elizabeth left
    - asking for a <podcast name> and <podcast episode name>
    - fetching list of those podcasts
    - using Levenstein distance algorithm (which works bad) to match <podcast episode name> with the podcast title from the fetched podcast list
    - WIP

--- podcast.py
+++ podcast.py
@@ -1,33 +1,49 @@
 import requests
 import json
 from typing import List
+from string_distance import recursive_levenshtein
+
+
+print(recursive_levenshtein("bartek pacia", "bartk pcia"))
+print(recursive_levenshtein("bartek pacia", "bartek paci"))
 
 
 class PodcastEpisode:
     '''
     Represents a single podcast episode from the dev.to API.
     '''
+
     id = 0
     path = None
     title = None
 
+    # the constructor
     def __init__(self, id, path, title):
         self.id = id
-        self.path = path
+        self.path = f"https://dev.to{path}"
         self.title = title
 
+    # equivalent of toString()
+    def __str__(self):
+        return f"(PodcastEpisode) id={self.id} , path={self.path} , title={self.title}"
 
-def fetch_podcasts() -> List[PodcastEpisode]:
+
+def fetch_podcasts(username: str) -> List[PodcastEpisode]:
     '''
     Fetches a list of podcasts from the dev.to API.
     '''
-    response = requests.get("https://dev.to/api/podcast_episodes")
+    username = username.strip().replace(" ", "")
+    response = requests.get(
+        f"https://dev.to/api/podcast_episodes?username={username}")
     data = json.loads(response.text)
 
     podcast_episodes = []
+    if not data:
+        print("data is None. Something bad happened")
+
     for podcast_data in data:
         podcast_episode = PodcastEpisode(
-            podcast_data["id"], podcast_data["path"], podcast_data["title"]
+            podcast_data["id"], podcast_data["path"], podcast_data["title"].lower()
         )
 
         podcast_episodes.append(podcast_episode)
@@ -43,9 +59,22 @@ def save(podcast_file):
     pass
 
 
-# this will come from ZSH, I guess
-query_entered_by_the_user = "code newbie".lower()
+# this will come from ZSH, I guess. For now, let's just use input()
+query_entered_by_the_user = input("Enter podcast name: ").lower()
+# this might also come from ZSH side (?)
+podcast_episode_name = input("Enter podcast episode name: ").lower()
 
-episodes = fetch_podcasts()
-for podcast in episodes:
-    print(podcast.title)
+episodes = fetch_podcasts(query_entered_by_the_user)
+best_match_value = 0
+episode_with_best_match = None
+for podcast_episode in episodes:
+    match_value = recursive_levenshtein(
+        podcast_episode_name, podcast_episode.title)
+    if match_value > best_match_value:
+        best_match_value = match_value
+        episode_with_best_match = podcast_episode
+
+    print(f"match value for {podcast_episode.title}: {match_value}")
+
+
+print(f"{episode_with_best_match}: {match_value}")
