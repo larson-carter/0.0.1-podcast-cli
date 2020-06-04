a93a304614aca0199a08f8d62f01315ec31140c3
Lionell <lionell_loh@mymail.sutd.edu.sg>
2020-06-05 01:34:37 +0800

    nest mp3_files folder in a parent folder podcasts

--- download.py
+++ download.py
@@ -3,6 +3,7 @@ import urllib.request
 import os
 import sys
 from pathlib import Path
+import lxml.html
 
 def download_podcast(directory, url_path):
     '''
@@ -10,11 +11,24 @@ def download_podcast(directory, url_path):
     downloads the mp3 file to local machine in specified dir.
     '''
     full_url = f"https://dev.to/{url_path}"
+    res = requests.get(full_url)
+
+    if res.status_code != 200:
+        print(f"Status Code: {res.status_code} for {full_url}")
+        exit(1)
+
+    tree = lxml.html.fromstring(res.text)
+    for element in tree.xpath("//*[@src]"):
+        if element.attrib['src'][-3:] == "mp3":
+            mp3_url = element.attrib['src']
+
+    if not mp3_url:
+        print(f"MP3 URL not found")
 
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     urllib.request.install_opener(opener)
-    return urllib.request.urlretrieve(full_url, f"{directory}/{url_path}.mp3") 
+    return urllib.request.urlretrieve(full_url, f"{directory}/{url_path}")
 
 
 def create_dir(directory):
@@ -23,14 +37,17 @@ def create_dir(directory):
     '''
 
     if not os.path.exists(directory):
-        os.system(f"mkdir {directory}")
+        os.system(f"mkdir -p {directory}")
 
 
 if __name__ == "__main__":
-    directory = os.path.expanduser("~/mp3_files")
+    # print(len(sys.argv))
+    directory = os.path.expanduser("~/podcasts/mp3_files")
+    print(directory)
     create_dir(directory)
     podcast_name, episode_name = os.path.split(sys.argv[1]) 
     if podcast_name != None:
         create_dir(directory + "/" + podcast_name)
+
     download_podcast(directory, sys.argv[1])
 
