c17d242577a6dc39a8d1f6d25f929b12fa0920de
Elizabeth Tackett <emctackett@gmail.com>
2020-06-04 11:30:34 -0500

    download and save functionality

--- download.py
+++ download.py
@@ -2,12 +2,19 @@
 import requests
 import urllib.request
 import os
+import sys
 from pathlib import Path
 
-# WIP
-def download_podcast(url_path, destination):
+directory = os.path.expanduser("~/mp3_files")
+
+# custom_path = print(sys.argv)[1]
+# print(f"custom path: {custom_path}")
+
+url_path = "/iphreaks/ips-292-evolving-apps-and-hacking-around-with-eric-crichlow"  # This will come from the ZSH
+
+def download_podcast(url_path):
     '''
-    accepts path of podcast episode, and destination for download
+    accepts path of podcast episode, and {directory} for download
     downloads the mp3 file to local machine in specified dir.
     '''
     full_url = f"https://dev.to/{url_path}"
@@ -15,19 +22,22 @@ def download_podcast(url_path, destination):
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-agen', 'Mozilla/5.0')]
     urllib.request.install_opener(opener)
-    urllib.request.urlretrieve(full_url, destination)
+    return urllib.request.urlretrieve(full_url, f"{directory}/test-file.mp3") # CHANGE IT LATER
 
 
-url_path = "/iphreaks/ips-292-evolving-apps-and-hacking-around-with-eric-crichlow"
+def create_dir():
+    '''
+    Attempts to save the downloaded podcast file to the location {directory}
+    '''
 
-directory = os.path.expanduser("~/mp3_files")
+    if not os.path.exists(directory):
+        print("Directory does not exist. Creating...")
+        os.system(f"mkdir {directory}")
+    else:
+        print("Directory does exist.")
 
-if not os.path.exists(directory):
-    print("Directory does not exist. Creating...")
-   #  os.mkdir(os.path.expanduser("~/Desktop/downloaded"))
 
-    os.system(f"mkdir {directory}")
-else:
-    print("Directory does exist.")
+create_dir()
+print(download_podcast(url_path))
 
-download_podcast(url_path, f"{directory}/test_file.mp3")
+# zsh podcast.zsh https://dev.to/iphreaks/ips-292-evolving-apps-and-hacking-around-with-eric-crichlow ./my_custom_dir
