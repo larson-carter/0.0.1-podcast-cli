d018ac5c2568d83adfdd8ccd4719fec2d2b3d792
Elizabeth Tackett <emctackett@gmail.com>
2020-06-03 19:01:54 -0500

    add fetch_random_podcast

--- podcast.py
+++ podcast.py
@@ -33,6 +33,7 @@ class PodcastEpisode:
     def __str__(self):
         return f"(PodcastEpisode) id={self.id} , path={self.path} , title={self.title}"
 
+
 def parse_podcast_data(data):
     '''
     reusable method parses podcast json data returned by Dev.to API
@@ -51,6 +52,7 @@ def parse_podcast_data(data):
 
     return podcast_episodes
 
+
 def fetch_podcasts_by_title(username: str) -> List[PodcastEpisode]:
     '''
     Fetches a list of podcasts from the dev.to API.
@@ -64,19 +66,8 @@ def fetch_podcasts_by_title(username: str) -> List[PodcastEpisode]:
         f"https://dev.to/api/podcast_episodes?username={username}")
     data = json.loads(response.text)
 
-    # podcast_episodes = []
-    # if not data:
-    #     print("data is None. Something bad happened")
-    #
-    # for podcast_data in data:
-    #     podcast_episode = PodcastEpisode(
-    #         podcast_data["id"], podcast_data["path"], podcast_data["title"].lower()
-    #     )
-    #
-    #     podcast_episodes.append(podcast_episode)
-
-    #return podcast_episodes
     return parse_podcast_data(data)
+
 
 def fetch_random_podcast():
     '''
@@ -84,7 +75,13 @@ def fetch_random_podcast():
     random podcast episode from list of episodes returned by API.
     '''
 
-    pass
+    response = requests.get(
+        "https://dev.to/api/podcast_episodes")
+    data = json.loads(response.text)
+    podcast_episode_list = parse_podcast_data(data)
+    random_episode = random.choice(podcast_episode_list)
+
+    return random_episode
 
 
 def save(podcast_file):
@@ -99,27 +96,29 @@ def save(podcast_file):
 
 
 # this will come from ZSH, I guess. For now, let's just use input().
-query_entered_by_the_user = input("Enter podcast name: ").lower()
+# query_entered_by_the_user = input("Enter podcast name: ").lower()
 # this might also come from ZSH side (?)
-podcast_episode_name = input("Enter podcast episode name (empty input will return all): ").lower()
+# podcast_episode_name = input("Enter podcast episode name (empty input will return all): ").lower()
 
-episodes = fetch_podcasts_by_title(query_entered_by_the_user)
+# episodes = fetch_podcasts_by_title(query_entered_by_the_user)
 
+episode = fetch_random_podcast()
+print(episode.title)
 
-best_match_value = 0
-episode_with_best_match = None
-
-for podcast_episode in episodes:
-    match_value = recursive_levenshtein(
-        podcast_episode_name, podcast_episode.title)
-
-    if match_value > best_match_value:
-        best_match_value = match_value
-        episode_with_best_match = podcast_episode
-
-    # Used for debugging and testing how string similarity functionality works.
-    # Currently it works *bad*.
-    print(f"match value for {podcast_episode.title}: {match_value}")
-
-
-print(f"{episode_with_best_match}: {match_value}")
+# best_match_value = 0
+# episode_with_best_match = None
+#
+# for podcast_episode in episodes:
+#     match_value = recursive_levenshtein(
+#         podcast_episode_name, podcast_episode.title)
+#
+#     if match_value > best_match_value:
+#         best_match_value = match_value
+#         episode_with_best_match = podcast_episode
+#
+#     # Used for debugging and testing how string similarity functionality works.
+#     # Currently it works *bad*.
+#     print(f"match value for {podcast_episode.title}: {match_value}")
+#
+#
+# print(f"{episode_with_best_match}: {match_value}")
